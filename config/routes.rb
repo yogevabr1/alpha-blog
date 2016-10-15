56c8e0dd1a607f43c0bd7346a33c0dfa610efe19
Yogev <yogevabr@gmail.com>
2016-10-15 09:40:48 +0000

    Create article action

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   root 'pages#home'
   get 'about', to: 'pages#about'
+  resources :articles
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
   # Example of named route that can be invoked with purchase_url(id: product.id)
