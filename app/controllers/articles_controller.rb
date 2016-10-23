e957ba81d16f2858a7410df7de229f0ec8a3eb17
Yogev <yogevabr@gmail.com>
2016-10-23 08:18:54 +0000

    add a lot of stuffgit add .

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     if @article.save
       #do something
-      flash[:notice] = "Article was successfully created"
+      flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
     else
       render :new
@@ -30,13 +30,13 @@ class ArticlesController < ApplicationController
   
   def destroy
     @article.destroy
-    flash[:notice] = "Article was successfully deleted"
+    flash[:danger] = "Article was successfully deleted"
     redirect_to articles_path
   end
   
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article was sucessfully updated"
+      flash[:success] = "Article was sucessfully updated"
       redirect_to article_path(@article)
     else
       render :edit
